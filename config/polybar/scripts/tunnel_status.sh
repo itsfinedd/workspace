4d6a92798dca6ef8d3e9996728a42a8447aae9da
itsfinedd <159060442+itsfinedd@users.noreply.github.com>
2024-05-18 20:22:25 -0500

    Update tunnel_status.sh

--- config/polybar/scripts/tunnel_status.sh
+++ config/polybar/scripts/tunnel_status.sh
@@ -2,7 +2,7 @@
 STATUS=$(ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 
 if [ "$STATUS" = "tun0" ]; then
-  echo "$(ifconfig | grep "inet " | awk '{print $2}')"
+  echo "$(ifconfig tun0 | grep "inet " | awk '{print $2}')"
 else
   echo "Disconnected"
 fi
